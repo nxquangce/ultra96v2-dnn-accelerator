fc13c3858eaf92ab76dbde18c9dfc0af09175512
Quang Ng <nxquangce@gmail.com>
2021-03-18 16:25:04 +0700

    add input buffer; mod conv engine: add input buffer, ins 3 kc PEs, connect buffer - PEs

---
+++ src/input_buffer.v
@@ -0,0 +1,143 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Computer Engineering Lab - CSE - HCMUT
+// Engineer: Nguyen Xuan Quang
+// 
+// Create Date: 03/18/2021 02:52:36 PM
+// Design Name: Activation Input Buffer
+// Module Name: input_buffer
+// Project Name: ultra96v2-dnn-accelerator
+// Target Devices: ultra96v2
+// Tool Versions: 2018.2
+// Description: Buffer for activation input. 3 positions, 3 channel, max 8 positions
+// 
+// Dependencies:
+//  fifo_p1o3
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module input_buffer(
+    clk,
+    rst,
+    i_data_ch0,
+    i_data_ch0_val,
+    i_data_ch1,
+    i_data_ch1_val,
+    i_data_ch2,
+    i_data_ch2_val,
+    o_data_req,
+    o_data_ch0,
+    o_data_ch0_val,
+    o_data_ch1,
+    o_data_ch1_val,
+    o_data_ch2,
+    o_data_ch2_val,
+    data_counter_ch0,
+    data_counter_ch1,
+    data_counter_ch2
+    );
+
+////////////////////////////////////////////////////////////////////////////////
+// Parameter declarations
+parameter DAT_WIDTH     = 8;
+parameter NUM_CHANNEL   = 3;
+parameter NUM_RDATA     = 3;
+parameter FF_DEPTH      = 8;
+parameter FF_ADDR_WIDTH = 3;
+
+////////////////////////////////////////////////////////////////////////////////
+// Port declarations
+input  wire                                 clk;
+input  wire                                 rst;
+input  wire [DAT_WIDTH - 1 : 0]             i_data_ch0;
+input  wire                                 i_data_ch0_val;
+input  wire [DAT_WIDTH - 1 : 0]             i_data_ch1;
+input  wire                                 i_data_ch1_val;
+input  wire [DAT_WIDTH - 1 : 0]             i_data_ch2;
+input  wire                                 i_data_ch2_val;
+input  wire                                 o_data_req;
+output wire [DAT_WIDTH * NUM_RDATA - 1 : 0] o_data_ch0;
+output wire                                 o_data_ch0_val;
+output wire [DAT_WIDTH * NUM_RDATA - 1 : 0] o_data_ch1;
+output wire                                 o_data_ch1_val;
+output wire [DAT_WIDTH * NUM_RDATA - 1 : 0] o_data_ch2;
+output wire                                 o_data_ch2_val;
+output wire [FF_ADDR_WIDTH - 1 : 0]         data_counter_ch0;
+output wire [FF_ADDR_WIDTH - 1 : 0]         data_counter_ch1;
+output wire [FF_ADDR_WIDTH - 1 : 0]         data_counter_ch2;
+
+////////////////////////////////////////////////////////////////////////////////
+// Local logic and instantiation
+
+// Channel 0
+fifo_p1o3
+    #(
+    .NUM_RDATA     (NUM_RDATA),
+    .DAT_WIDTH     (DAT_WIDTH),
+    .FF_DEPTH      (FF_DEPTH),
+    .FF_ADDR_WIDTH (FF_ADDR_WIDTH)
+    )
+fifo_ch0
+    (
+    .clk          (clk),
+    .rst          (rst),
+    .wr_req       (i_data_ch0_val),
+    .wr_data      (i_data_ch0),
+    .rd_req       (o_data_req),
+    .rd_data      (o_data_ch0),
+    .rd_data_val  (o_data_ch0_val),
+    .data_counter (data_counter_ch0),
+    .full         (),
+    .empty        ()
+    );
+
+// Channel 1
+fifo_p1o3 
+    #(
+    .NUM_RDATA     (NUM_RDATA),
+    .DAT_WIDTH     (DAT_WIDTH),
+    .FF_DEPTH      (FF_DEPTH),
+    .FF_ADDR_WIDTH (FF_ADDR_WIDTH)
+    )
+fifo_ch1
+    (
+    .clk          (clk),
+    .rst          (rst),
+    .wr_req       (i_data_ch1_val),
+    .wr_data      (i_data_ch1),
+    .rd_req       (o_data_req),
+    .rd_data      (o_data_ch1),
+    .rd_data_val  (o_data_ch1_val),
+    .data_counter (data_counter_ch1),
+    .full         (),
+    .empty        ()
+    );
+
+// Channel 2
+fifo_p1o3 
+    #(
+    .NUM_RDATA     (NUM_RDATA),
+    .DAT_WIDTH     (DAT_WIDTH),
+    .FF_DEPTH      (FF_DEPTH),
+    .FF_ADDR_WIDTH (FF_ADDR_WIDTH)
+    )
+fifo_ch2
+    (
+    .clk          (clk),
+    .rst          (rst),
+    .wr_req       (i_data_ch2_val),
+    .wr_data      (i_data_ch2),
+    .rd_req       (o_data_req),
+    .rd_data      (o_data_ch2),
+    .rd_data_val  (o_data_ch2_val),
+    .data_counter (data_counter_ch2),
+    .full         (),
+    .empty        ()
+    );
+
+endmodule
--- src/line_kcpe_conv2d_engine.v
+++ src/line_kcpe_conv2d_engine.v
@@ -44,27 +44,141 @@ parameter NUM_KERNEL    = 4;
 parameter NUM_KCPE      = 3;    // Number of kernel-channel PE
 parameter REG_WIDTH     = 32;
 
+parameter NUM_RDATA     = 3;
+
 ////////////////////////////////////////////////////////////////////////////////
 // Port declarations
-input  wire                                                clk;
-input  wire                                                rst;
-input  wire [(BIT_WIDTH * NUM_CHANNEL * NUM_KCPE) - 1 : 0] i_data;
-input  wire [(BIT_WIDTH * NUM_KERNEL  * NUM_KCPE) - 1 : 0] i_weight;
-input  wire [(BIT_WIDTH * NUM_KERNEL            ) - 1 : 0] i_psum;
-input  wire                                                i_data_val;
-input  wire                                                i_weight_val;
-input  wire                                                i_psum_val;
-output wire [(BIT_WIDTH * NUM_KERNEL            ) - 1 : 0] o_psum;
-output wire [NUM_KERNEL - 1 : 0]                           o_psum_val;
-output wire [REG_WIDTH * NUM_KCPE - 1 : 0]                 err_psum_val;
+input  wire                                     clk;
+input  wire                                     rst;
+input  wire [(BIT_WIDTH * NUM_CHANNEL) - 1 : 0] i_data;
+input  wire [(BIT_WIDTH * NUM_KERNEL ) - 1 : 0] i_weight;
+input  wire [(BIT_WIDTH * NUM_KERNEL ) - 1 : 0] i_psum;
+input  wire                                     i_data_val;
+input  wire                                     i_weight_val;
+input  wire                                     i_psum_val;
+output wire [(BIT_WIDTH * NUM_KERNEL ) - 1 : 0] o_psum;
+output wire [NUM_KERNEL - 1 : 0]                o_psum_val;
+// output wire [REG_WIDTH * NUM_KCPE - 1 : 0]      err_psum_val;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Local logic and instantiation
-kernel_channel_pe kernel_channel_pe_0(
+wire [BIT_WIDTH - 1 : 0] i_data_ch0;
+wire [BIT_WIDTH - 1 : 0] i_data_ch1;
+wire [BIT_WIDTH - 1 : 0] i_data_ch2;
+wire [BIT_WIDTH * NUM_RDATA - 1 : 0] buffer_o_data_ch0;
+wire [BIT_WIDTH * NUM_RDATA - 1 : 0] buffer_o_data_ch1;
+wire [BIT_WIDTH * NUM_RDATA - 1 : 0] buffer_o_data_ch2;
+wire                                 buffer_o_data_ch0_val;
+wire                                 buffer_o_data_ch1_val;
+wire                                 buffer_o_data_ch2_val;
+wire [BIT_WIDTH * NUM_CHANNEL - 1 : 0] engine_i_data_pos0;
+wire [BIT_WIDTH * NUM_CHANNEL - 1 : 0] engine_i_data_pos1;
+wire [BIT_WIDTH * NUM_CHANNEL - 1 : 0] engine_i_data_pos2;
+
+// Split input data into channels
+assign i_data_ch0 = i_data[BIT_WIDTH     - 1 : 0];
+assign i_data_ch1 = i_data[BIT_WIDTH * 2 - 1 : BIT_WIDTH];
+assign i_data_ch2 = i_data[BIT_WIDTH * 3 - 1 : BIT_WIDTH * 2];
+
+// Activation input buffer
+input_buffer input_buffer_0(
+    .clk              (clk),
+    .rst              (rst),
+    .i_data_ch0       (i_data_ch0),
+    .i_data_ch0_val   (i_data_val),
+    .i_data_ch1       (i_data_ch1),
+    .i_data_ch1_val   (i_data_val),
+    .i_data_ch2       (i_data_ch2),
+    .i_data_ch2_val   (i_data_val),
+    .o_data_req       (),
+    .o_data_ch0       (buffer_o_data_ch0),
+    .o_data_ch0_val   (buffer_o_data_ch0_val),
+    .o_data_ch1       (buffer_o_data_ch1),
+    .o_data_ch1_val   (buffer_o_data_ch1_val),
+    .o_data_ch2       (buffer_o_data_ch2),
+    .o_data_ch2_val   (buffer_o_data_ch2_val),
+    .data_counter_ch0 (),
+    .data_counter_ch1 (),
+    .data_counter_ch2 ()
+    );
+
+// Convert position/channel into channel/position
+wire [BIT_WIDTH - 1 : 0] buffer_o_data [NUM_KCPE - 1 : 0][NUM_CHANNEL - 1 : 0];
+
+genvar idxPos;
+generate
+    for (idxPos = 0; idxPos < NUM_KCPE; idxPos = idxPos + 1) begin
+        assign buffer_o_data[idxPos][0] = buffer_o_data_ch0[BIT_WIDTH - 1 : 0];
+        assign buffer_o_data[idxPos][1] = buffer_o_data_ch1[BIT_WIDTH * 2 - 1 : BIT_WIDTH];
+        assign buffer_o_data[idxPos][2] = buffer_o_data_ch2[BIT_WIDTH * 3 - 1 : BIT_WIDTH * 2];
+    end
+endgenerate
+
+// Data in position
+assign engine_i_data_pos0 = {buffer_o_data[0][2], buffer_o_data[0][1], buffer_o_data[0][0]};
+assign engine_i_data_pos1 = {buffer_o_data[1][2], buffer_o_data[1][1], buffer_o_data[1][0]};
+assign engine_i_data_pos2 = {buffer_o_data[2][2], buffer_o_data[2][1], buffer_o_data[2][0]};
+
+// KC PE 0
+kernel_channel_pe 
+    #(
+    .NUM_CHANNEL    (NUM_CHANNEL),
+    .BIT_WIDTH      (BIT_WIDTH),
+    .NUM_KERNEL     (NUM_KERNEL),
+    .REG_WIDTH      (REG_WIDTH)
+    )
+kernel_channel_pe_0
+    (
     .clk            (clk),
     .rst            (rst),
-    .i_data         (),
-    .i_data_val     (),
+    .i_data         (engine_i_data_pos0),
+    .i_data_val     (buffer_o_data_ch0_val),
+    .i_weight       (),
+    .i_weight_val   (),
+    .i_psum         (),
+    // .i_psum_val     (),
+    .o_psum         (),
+    .o_psum_val     (),
+    .err_psum_val   ()
+    );
+
+// KC PE 1
+kernel_channel_pe 
+    #(
+    .NUM_CHANNEL    (NUM_CHANNEL),
+    .BIT_WIDTH      (BIT_WIDTH),
+    .NUM_KERNEL     (NUM_KERNEL),
+    .REG_WIDTH      (REG_WIDTH)
+    )
+kernel_channel_pe_1
+    (
+    .clk            (clk),
+    .rst            (rst),
+    .i_data         (engine_i_data_pos1),
+    .i_data_val     (buffer_o_data_ch1_val),
+    .i_weight       (),
+    .i_weight_val   (),
+    .i_psum         (),
+    // .i_psum_val     (),
+    .o_psum         (),
+    .o_psum_val     (),
+    .err_psum_val   ()
+    );
+
+// KC PE 2
+kernel_channel_pe 
+    #(
+    .NUM_CHANNEL    (NUM_CHANNEL),
+    .BIT_WIDTH      (BIT_WIDTH),
+    .NUM_KERNEL     (NUM_KERNEL),
+    .REG_WIDTH      (REG_WIDTH)
+    )
+kernel_channel_pe_2
+    (
+    .clk            (clk),
+    .rst            (rst),
+    .i_data         (engine_i_data_pos2),
+    .i_data_val     (buffer_o_data_ch2_val),
     .i_weight       (),
     .i_weight_val   (),
     .i_psum         (),
